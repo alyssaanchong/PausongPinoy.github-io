000c0828ade126b4f8237680721d4887e76e8cb0
alyssaanchong <alyssaayinlove123@gmail.com>
2016-10-17 15:47:52 +0800

    Add files via upload

---
+++ Deees.css
@@ -0,0 +1,19 @@
+nav li {
+  padding: 0 50px;
+  display: inline;
+}
+
+nav a {
+  color: black;
+  text-decoration: none;
+  font-size: 30px;
+}
+img{
+	width: 42;
+	height: 42;
+}
+#ito{
+	padding-top: 0 50px;
+	padding-bottom: 0 50px;
+	margin-top: 30px;
+}
